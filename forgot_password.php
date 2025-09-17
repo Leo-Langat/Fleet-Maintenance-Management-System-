82142b41e5bda23bdd17d48930804cc7d0a241cf
Lionel Langat <Lionel.Langat@epra.go.ke>
2025-09-17 14:12:12 +0300

    Updated the fumction SendEmail in forgot_password.php the

--- forgot_password.php
+++ forgot_password.php
@@ -56,8 +56,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $mail->isSMTP();
                 $mail->Host       = 'smtp.gmail.com';
                 $mail->SMTPAuth   = true;
-                $mail->Username   = 'lionellangat2000@gmail.com';
-                $mail->Password   = 'srsl vbwk afvc wwbs'; 
+                $mail->Username   = 'test@gmail.com';
+                $mail->Password   = '**** **** **** ****'; 
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port       = 587;
                 $mail->setFrom('lionellangat2000@gmail.com', 'Mutai Enterprises Ltd');
